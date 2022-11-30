be538d055f923e2e58d5b10a0c0e0f8b5675b11d
Niloys7 <niloys7live@gmail.com>
2022-11-30 13:30:04 +0600

    Update ran-client.php

--- ran-client.php
+++ ran-client.php
@@ -8,5 +8,5 @@
 require_once 'class-remote-notification-client.php';
 
 if ( function_exists( 'wpi_rdnc_add_notification' ) ) {
-	wpi_rdnc_add_notification( 72, '30e3b659ed1aba30', 'https://wpinteractive.com' );
+	wpi_rdnc_add_notification( 31, '7cfcbdd88059b2ce', 'https://test.local' );
 }
